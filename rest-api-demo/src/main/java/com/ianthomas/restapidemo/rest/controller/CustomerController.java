b7a8c359e31daad2ba33f9ef44db8ce2802af1a3
Ian Thomas <ian@spectrumware.net>
2022-03-03 15:08:43 -0600

    Started exception handling in service/controller layers

--- rest-api-demo/src/main/java/com/ianthomas/restapidemo/rest/controller/CustomerController.java
+++ rest-api-demo/src/main/java/com/ianthomas/restapidemo/rest/controller/CustomerController.java
@@ -1,10 +1,12 @@
 package com.ianthomas.restapidemo.rest.controller;
 
+import com.ianthomas.restapidemo.persistence.exception.ApplicationException;
 import com.ianthomas.restapidemo.persistence.model.Customer;
 import com.ianthomas.restapidemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.http.HttpResponse;
 import java.util.List;
 
 @RestController
@@ -25,22 +27,40 @@ public class CustomerController {
 
     @PostMapping
     public void addCustomer(@RequestBody Customer customer) {
-        if (customer.getId() < 0) { throw new IllegalStateException("Invalid: id must be a positive value"); }
-        customerService.addCustomer(customer);
+        try {
+            customerService.addCustomer(customer);
+        } catch (ApplicationException ae){
+
+        } catch (Exception e){
+        }
     }
 
     @PutMapping(path = "{id}")
     public void updateCustomer(@PathVariable("id") Integer id,
                                @RequestParam(required = false) String name,
                                @RequestParam(required = false) String email) {
-        if (id < 0) { throw new IllegalStateException("Invalid: id must be a positive value"); }
-        customerService.updateCustomer(id, name, email);
+        try {
+            customerService.updateCustomer(id, name, email);
+            // Statement 1
+            // Statement 2
+            // Statement 3
+        } catch (ApplicationException ae){
+            // Any error thrown from Service layer is caught here
+        } catch (Exception e){
+            // logger.error(e)
+            throw HttpResponse<>
+        }
     }
 
     @DeleteMapping(path = "{id}")
     public void deleteCustomer(@PathVariable("id") Integer id) {
-        if (id < 0) { throw new IllegalStateException("Invalid: id must be a positive value"); }
-        customerService.deleteCustomer(id);
+        try {
+            customerService.deleteCustomer(id);
+        } catch (ApplicationException ae){
+
+        } catch (Exception e){
+
+        }
     }
 
 
--- rest-api-demo/src/main/java/com/ianthomas/restapidemo/service/CustomerService.java
+++ rest-api-demo/src/main/java/com/ianthomas/restapidemo/service/CustomerService.java
@@ -1,5 +1,6 @@
 package com.ianthomas.restapidemo.service;
 
+import com.ianthomas.restapidemo.persistence.exception.ApplicationException;
 import com.ianthomas.restapidemo.persistence.model.Customer;
 import com.ianthomas.restapidemo.persistence.model.Inventory;
 import com.ianthomas.restapidemo.persistence.repository.CustomerRepository;
@@ -29,39 +30,72 @@ public class CustomerService {
         return customerRepository.findCustomersById();
     }
 
-    public void addCustomer(Customer customer) {
-        Optional<Customer> idMatch = customerRepository.findCustomerById(customer.getId());
-        if (idMatch.isPresent()){
-            throw new IllegalStateException("Customer id " + customer.getId() + " already exists");
+    public void addCustomer(Customer customer) throws ApplicationException {
+        try {
+            if (customer.getId() < 0) {
+                throw new ApplicationException("Invalid: id must be a positive value");
+            }
+            Optional<Customer> idMatch = customerRepository.findCustomerById(customer.getId());
+            if (idMatch.isPresent()) {
+                throw new ApplicationException("Customer id " + customer.getId() + " already exists");
+            }
+            Optional<Customer> emailMatch = customerRepository.findCustomerByEmail(customer.getEmail());
+            if (emailMatch.isPresent()) {
+                throw new ApplicationException("Customer email " + customer.getEmail() + " already exists");
+            }
+            customerRepository.save(customer);
+        } catch (ApplicationException ae){
+            throw ae;
+        } catch (Exception e) {
+
         }
-        Optional<Customer> emailMatch = customerRepository.findCustomerByEmail(customer.getEmail());
-        if (emailMatch.isPresent()) {
-            throw new IllegalStateException("Customer email " + customer.getEmail() + " already exists");
-        }
-        customerRepository.save(customer);
     }
 
     @Transactional
-    public void updateCustomer(int id, String name, String email) {
-        Customer customer = customerRepository.findById(id)
-                .orElseThrow(() -> new IllegalStateException("Customer id " + id + " does not exist"));
+    public void updateCustomer(int id, String name, String email) throws ApplicationException {
+        try {
+            if (id < 0) {
+                throw new ApplicationException("Invalid: id must be a positive value");
+            }
+            Customer customer = customerRepository.findById(id)
+                    .orElseThrow(() -> new ApplicationException("Customer id " + id + " does not exist"));
 
-        if (!Objects.equals(customer.getName(), name) && name.length() > 0) {
-            customer.setName(name);
-        }
-        if (!Objects.equals(customer.getEmail(), email) && email.length() > 0) {
-            customer.setEmail(email);
-        }
+            if (!Objects.equals(customer.getName(), name) && name.length() > 0) {
+                customer.setName(name);
+            }
+            if (!Objects.equals(customer.getEmail(), email) && email.length() > 0) {
+                customer.setEmail(email);
+            }
 
-        System.out.println("Updated customer: " + id + ".");
+            System.out.println("Updated customer: " + id + ".");
+        } catch (ApplicationException ae) {
+            throw ae;
+        } catch (Exception e) {
+            // Log database error
+            // logger.error(e)
+            ApplicationException ae = new ApplicationException(e);
+            throw ae;
+        }
     }
 
-    public void deleteCustomer(Integer id) {
-        boolean exists = customerRepository.existsById(id);
-        if (exists){
-            customerRepository.deleteById(id);
-        }
+    public void deleteCustomer(Integer id) throws ApplicationException {
+        try {
+            if (id < 0) {
+                throw new ApplicationException("Invalid: id must be a positive value");
+            }
+            boolean exists = customerRepository.existsById(id);
+            if (exists) {
+                customerRepository.deleteById(id);
+            }
 
-        System.out.println("Deleted customer: " + id + ".");
+            System.out.println("Deleted customer: " + id + ".");
+        } catch (ApplicationException ae) {
+            throw ae;
+        } catch (Exception e) {
+            // Log database error
+            // logger.error(e)
+            ApplicationException ae = new ApplicationException(e);
+            throw ae;
+        }
     }
 }
